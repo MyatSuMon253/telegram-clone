661d15e39635002c1c6110d76e42db188ead78e1
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-04-06 15:03:55 +0630

    feat: create avatar

--- app/components/login/Avatar.tsx
+++ app/components/login/Avatar.tsx
@@ -1,9 +1,25 @@
+'use client'
 
+import { AvatarProps } from "@/app/types";
+import Image from "next/image";
 
-const Avatar = () => {
+const Avatar = ({ avatarId, setAvatarId }: AvatarProps) => {
   return (
-    <div>Avatar</div>
-  )
-}
+    <div
+      className="avatar cursor-pointer mx-auto mb-5 tooltip"
+      data-tip="Click to regenerate new avatar"
+      onClick={() => setAvatarId((Math.random() * 20).toFixed())}
+    >
+      <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+        <Image
+          src={`https://robohash.org/${avatarId}.png`}
+          width={256}
+          height={256}
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Avatar
+export default Avatar;
